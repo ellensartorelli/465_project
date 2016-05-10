bf5876184a1a8972c81810a6d08a2e54c44a7245
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2016-05-10 16:56:34 -0400

    add nest.js. contains function to nest data from geojson

---
+++ nest.js
@@ -0,0 +1,19 @@
+function nest(data) {
+  return data.features.map(function(element) {
+    var oldProperties = element.properties;
+    element.properties = {
+      BoroName : oldProperties.BoroName,
+      NTAName : oldProperties.NTAName,
+      id : oldProperties.id
+    };
+    ["black", "mhinc", "mhv", "mrent", "pcol", "pop", "white"].forEach(function(word){
+      var x = {};
+      ["1970", "1980", "1990", "2000", "2010"].forEach(function(year){
+        var abbr = year.slice(-2);
+        x[year] = oldProperties[word+"_"+abbr];
+      })
+      element.properties[word] = x;
+    })
+    return element;
+  })
+}
