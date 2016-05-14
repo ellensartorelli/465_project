9668a7b7cd39cac33fd50ad430b6193ccc3f8f8b
Ellen Sartorelli <esartorelli@middlebury.edu>
2016-05-14 14:27:53 -0400

    add slider

---
+++ scripts/slider.js
@@ -0,0 +1,20 @@
+function createSlider(parent, width){
+	// var svg = d3.select(parent);
+		// .append("svg")
+		// .attr({"width":width, "height":height});
+
+	//create slider
+	var slider_svg = d3.select(parent)
+		.append("svg")
+		.attr({"width":width, "height":100})
+		.attr("id", "slider");
+
+	// Initialize slider
+	var slider = d3.slider().min(1970).max(2010).ticks(5)
+		.stepValues([1970,1980,1990,2000,2010])
+		.tickFormat(d3.format("d"));
+	// Render the slider in the div
+	d3.select('#slider').call(slider);
+};
+
+
