e6c7cbcc889f80d923596c486fa5f16678e49c05
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2016-05-14 11:03:07 -0400

    fix nest.js to rename variables from the topojson

--- scripts/nest.js
+++ scripts/nest.js
@@ -1,4 +1,43 @@
 function nest(array) {
+
+  oldWords = {
+    "black": {
+      "1970" : "70_BLACK70",
+      "1980": "80_NHBLK80",
+      "1990": "90_NHBLK90",
+      "2000": "00_NHBLK00",
+      "2010": "10_nhblk10"
+    },
+    "mhv": {
+      "1970" : "70_MHMVAL70",
+      "1980": "80_MHMVAL80",
+      "1990": "90_MHMVAL90",
+      "2000": "00_MHMVAL00",
+      "2010": "10_mhmval0a"
+    },
+    "mrent": {
+      "1970" : "70_MRENT70",
+      "1980": "80_MRENT80",
+      "1990": "90_MRENT90",
+      "2000": "00_MRENT00",
+      "2010": "10_mrent0a"
+    },
+    "pop": {
+      "1970" : "70_POP70",
+      "1980": "80_POP80",
+      "1990": "90_POP90",
+      "2000": "00_POP00",
+      "2010": "10_pop10"
+    },
+    "white": {
+      "1970" : "70_WHITE70",
+      "1980": "80_NHWHT80",
+      "1990": "90_NHWHT90",
+      "2000": "00_NHWHT00",
+      "2010": "10_nhwht10"
+    }
+  };
+
   return array.map(function(element) {
     var oldProperties = element.properties;
     element.properties = {
@@ -9,8 +48,13 @@ function nest(array) {
     ["black", "mhinc", "mhv", "mrent", "pcol", "pop", "white"].forEach(function(word){
       var x = {};
       ["1970", "1980", "1990", "2000", "2010"].forEach(function(year){
-        var abbr = year.slice(-2);
-        x[year] = oldProperties[word+"_"+abbr];
+        var oldWord;
+        if (word == "mhinc" || word == "pcol") {
+          oldWord = word + "_" + year.slice(-2);
+        } else {
+          oldWord = oldWords[word][year];
+        }
+        x[year] = +oldProperties[oldWord];
       })
       element.properties[word] = x;
     })
